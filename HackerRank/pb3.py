12578fe3dbf8ae930310625c742b716c2edb3e83
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-08-22 18:47:43 +0100

    pb5

--- HackerRank/pb3.py
+++ HackerRank/pb3.py
@@ -22,3 +22,12 @@ def repeatedString(s, n):
     count=arr.count("a")
     result+=count
     return result
+
+def hourglassSum(arr):
+    # Write your code here  
+    smth=[]  
+    for i in range(4):  
+        for j in range(4):   
+                smth.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]) 
+
+    return max(smth)
