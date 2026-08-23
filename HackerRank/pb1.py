087bbbff9c5665cf7ed97866a182406137d148cc
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-08-23 19:21:12 +0100

    pb10

--- HackerRank/pb1.py
+++ HackerRank/pb1.py
@@ -20,3 +20,10 @@ def checkMagazine(magazine, note):
         magazine_count[word] -= 1
 
     print("Yes")
+
+def twoStrings(s1, s2):
+    dic1 = Counter(s1)
+    for letter in s2:
+        if dic1[letter] != 0:
+            return "YES"
+    return "NO"
