2d4aa5be283e92d8bd0262a4a263658599145eb2
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-08-23 13:15:24 +0100

    pb9

--- HackerRank/pb1.py
+++ HackerRank/pb1.py
@@ -8,3 +8,15 @@ def sockMerchant(n, ar):
         pairs += count // 2  
         
     return pairs
+
+def checkMagazine(magazine, note):
+    magazine_count = Counter(magazine)
+
+    for word in note:
+        if magazine_count[word] == 0:
+            print("No")
+            return
+
+        magazine_count[word] -= 1
+
+    print("Yes")
